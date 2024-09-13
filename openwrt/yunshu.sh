fe7a52a296fae86d425c2b51bece8e57755afb6c
yunshu <33997505+hello-yunshu@users.noreply.github.com>
2024-09-13 17:52:37 +0800

    Update yunshu.sh

--- openwrt/yunshu.sh
+++ openwrt/yunshu.sh
@@ -7,8 +7,9 @@ uci set dhcp.lan.dhcpv6=hybrid
 uci set dhcp.lan.ndp=hybrid
 uci set dhcp.lan.ra_management=1
 uci commit dhcp
-#uci delete firewall.@defaults[0].flow_offloading
-#uci commit firewall
+uci delete firewall.@defaults[0].flow_offloading
+uci set firewall.@defaults[0].natflow='1'
+uci commit firewall
 wait
 sleep 1
 service network restart
